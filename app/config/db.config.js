749c628392957820d3bd58f2aa50c55de2c9af7a
ricardor <ricardo88812968@gmail.com>
2023-07-10 10:16:17 -0400

    actualizaciones

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 const sequelize = new Sequelize(
     "db_bootcamp",
     "postgres",
-    "Dual2013", {
+    "", {
     host: "localhost",
     dialect: "postgres",
 });
